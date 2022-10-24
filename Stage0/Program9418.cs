17363914951059e4b50fda8de61fad1af0d25522
NaomiReitzer <nreitzer@g.jct.ac.il>
2022-10-24 20:43:31 +0300

    Initial code  + Welcome functions

---
+++ Stage0/Program9418.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9418();
+            Welcome1355();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1355();
+
+        private static void Welcome9418()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ", welcome to mt first console application");
+        }
+    }
+}
